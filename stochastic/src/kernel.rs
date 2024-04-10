d6ad38bec908d851aa4b1b2db433ad0aba0f536e
Kotaro Pindur <kotaro.pindur@gmail.com>
2024-04-11 04:12:26 +0900

    doc: add basic documentation

--- stochastic/src/kernel.rs
+++ stochastic/src/kernel.rs
@@ -1,12 +1,33 @@
 
-
+/// Enumeration representing different types of kernel functions
 pub enum Kernel {
+    /// Radial basis function (RBF) kernel.
+    /// It takes a single parameter representing the width or scale of the kernel
     Radial(f64),
+    /// Periodic kernel.
+    /// It takes a typle of two paramters representing the length scale and the periodicity of the kernel
     Periodic((f64, f64)),
+    /// Linear kernel.
+    /// It takes a tuple of parameters representing the slop and intercept of the linear function
     Linear((f64, f64))
 }
 
+/// Applies the specified kernel function to the input data point.
+///
+/// # Args:
+/// + 'xs' - A tuple representing the input data point
+/// + 'kernel' - The kernel function to be applied
+///
+/// # Return:
+/// + The result of applying the kernel function to the input data point
 /// 
+/// # Examples:
+/// ```
+/// let xs = (0.5, 0.8);
+/// let radial_kernel = Kernel::Radial(0.5);
+/// let result = apply(&xs, radial_kernel);
+/// println!("Result: {}", result);
+/// ```
 pub fn apply(xs: &(f64, f64), kernel: Kernel) -> f64 {
     match kernel {
         Kernel::Radial(length) => rbf(xs, &length),
